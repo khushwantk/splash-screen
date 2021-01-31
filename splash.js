6af153d4ffa0e69473970667024de0f1e0e58bcc
khushwantk <kk910946@gmail.com>
2021-01-31 18:50:57 +0530

    animation added

--- splash.js
+++ splash.js
@@ -1,3 +1,15 @@
+anime({
+    targets: "#amazing  path",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutQuad",
+    duration: 4000,
+    delay: function (el, i) {
+      return i * 100;
+    },
+    direction: "forward",
+    loop: false,
+  });
+
 const splash=document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded',(e)=>{
     setTimeout(()=>{
